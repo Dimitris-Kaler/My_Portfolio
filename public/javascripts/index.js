b561de354329a8241e5c29ac9f489cd70f5b34ce
ΚΑΛΕΡΑΝΤΕΣ ΔΗΜΗΤΡΗΣ <kalerantesd@interamerican.gr>
2022-09-10 13:15:33 +0300

    creating carusel for dislaying my skills,styling and functionality

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -7,9 +7,10 @@ var flashMsg=document.getElementsByClassName("msg-sent")[0]
 let delBtn=document.querySelector(".msg-sent .del")
 
 
+
+
 //*****Hamburger Open menu ******//
 links.forEach(link=>{
-    console.log(link)
     link.addEventListener("click",()=>{
     if (window.innerWidth > 1200) {
         hamburger.classList.remove("open-hamburger") 
@@ -44,6 +45,63 @@ for(let link of links){
 
 hamburger.addEventListener("click",open1);
 
+/****CARUSEL SKILLS */
+const sliders=document.querySelector(".skill-container");
+let skillcards=document.getElementsByClassName("skill-card");
+let scrollPerClick;
+let imagePadding=20;
+let scrollAmount=0
+let caruselLeftBtn=document.querySelector(".switchLeft")
+let caruselRightBtn=document.querySelector(".switchRight")
+let skillName=document.getElementById("skillName")
+let skillImage=document.getElementsByClassName("skill-image")
+
+scrollPerClick=skillcards[0].clientWidth+imagePadding
+
+
+
+
+
+caruselLeftBtn.addEventListener("click",sliderScrollLeft)
+caruselRightBtn.addEventListener("click",sliderScrollRight)
+
+
+
+function sliderScrollLeft(){
+    sliders.scrollTo({
+        top:0,
+        left:(scrollAmount-=scrollPerClick),
+        behavior:"smooth"
+    });
+    if(scrollAmount<0){
+        scrollAmount=0;
+    }
+}
+
+function sliderScrollRight(){
+    if(scrollAmount<=sliders.scrollWidth-sliders.clientWidth){
+        sliders.scrollTo({
+            top:0,
+            left:(scrollAmount+=scrollPerClick),
+            behavior:"smooth"
+        })
+    }
+}
+
+//APPEAR THE NAME OF THE SKILL AFTER HOVERING AT THE IMAGE
+
+Array.from(skillcards).forEach((card)=>{
+    card.addEventListener("mouseover",(event)=>{
+        skillName.innerHTML=event.target.alt;
+    })
+    card.addEventListener("mouseout",()=>{
+        skillName.innerHTML=""
+    })
+})
+
+
+
+
 
 
 //*****Flash-msg*****//
@@ -59,3 +117,6 @@ if(flashMsg.innerHTML.length>0){
 delBtn.addEventListener("click",(event)=>{
         event.target.parentNode.style.visibility="hidden"
 })
+
+
+
