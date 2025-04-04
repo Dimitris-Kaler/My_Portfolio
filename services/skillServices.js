234746b3356c4c4e2e111fc3fe2a2c7a5158c0e1
kalerantesd <kalerantesd@interamerican.gr>
2025-04-04 15:07:17 +0300

    change the skills section

--- services/skillServices.js
+++ services/skillServices.js
@@ -5,30 +5,43 @@ const html=new Skill("HTML","./images/html.png");
 
 const css=new Skill("CSS","./images/css.png")
 
-const javascript=new Skill("JAVASCRIPT","./images/javascript.png")
+const javascript=new Skill("JAVASCRIPT","./images/javascript-removebg-preview.png")
 
 const java=new Skill("JAVA","./images/java.png")
 
-const rpa=new Skill("ROBOTIC PROCESS AUTOMATION","./images/rpa.jpg")
+const rpa=new Skill("ROBOTIC PROCESS AUTOMATION","./images/rpa-removebg-preview.png")
 
-const bootstrap=new Skill("BOOTSPTRAP","./images/Bootstrap.png")
+const bootstrap=new Skill("BOOTSTRAP","./images/Bootstrap.png")
 
-const node=new Skill("NODE","./images/node.png");
+const node=new Skill("NODE","./images/node-removebg-preview.png");
 
-const express=new Skill("EXPRESS","./images/express.png")
+const express=new Skill("EXPRESS","./images/express-removebg-preview.png")
 
 const react=new Skill("REACT","./images/react.png")
 
-const sails=new Skill("SAILS","./images/sails.jpg")
+const sails=new Skill("SAILS","./images/sails-removebg-preview.png")
 
-const mysql=new Skill("MYSQL","./images/mysql.png");
+const sql=new Skill("SQL","./images/SQL-removebg-preview.png");
 
-const liberty=new Skill("OPEN LIBERTY","./images/liberty.jpg")
+const liberty=new Skill("OPEN LIBERTY","./images/liberty-removebg-preview.png")
+
+const git=new Skill("Git","./images/git.png")
+
+const webServices=new Skill("WebServices","./images/webservices.png")
+
+const quarkus=new Skill("Quarkus","./images/quarkus-removebg-preview.png")
+
+const keycloak=new Skill("Keycloak","./images/keycloak-removebg-preview.png")
+
+const OAuth2=new Skill("OAuth2","./images/oauth2-removebg-preview.png")
+
+const siebel=new Skill("Siebel CRM","./images/siebel.png")
 
 
 
 
-const skillsArray=[html,css,javascript,java,rpa,bootstrap,node,express,react,sails,mysql,liberty]
+
+const skillsArray=[html,css,javascript,java,rpa,bootstrap,node,express,react,sails,sql,liberty,git,webServices,quarkus,keycloak,OAuth2,siebel]
 
 
 
